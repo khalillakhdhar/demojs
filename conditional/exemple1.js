d0d9ff76f107b41b381ab50795c2335a9547c041
khalil <khalillakhdharatc@gmail.com>
2023-03-07 15:27:03 +0100

    Create exemple1.js

---
+++ conditional/exemple1.js
@@ -0,0 +1,12 @@
+function eval(age)
+{
+    if(age<=12)
+    console.log("vous êtes mineur!")
+    else if(age<=15)
+    console.log("vous jouez avec les cadets!")
+    else if(age<18)
+    console.log("vous êtes avec les juniors!")
+    else
+    console.log("vous êtes avec les sénior!")
+}
+eval(9)
